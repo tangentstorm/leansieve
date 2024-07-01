2e450c8fa74391f9d5d65d4dfe0747b9242df86b
tangentstorm <michal.wallace@gmail.com>
2024-07-01 11:11:58 -0400

    Make sequences callable like functions, define ids,evens,odds

--- Main.lean
+++ Main.lean
@@ -21,8 +21,23 @@ def ap (s : ASeq) (n : Nat) : Nat := s.k + s.d * n
 -- apply one formula to another: r(n) := s(t(n))
 def compose (s : ASeq) (t : ASeq) : ASeq := mk (s.k + s.d * t.k) (s.d * t.d)
 
--- identity series
-def ids : ASeq := mk 2 1
+-- identity sequence
+def ids : ASeq := mk 0 1
+
+def evens : ASeq := mk 0 2
+def odds : ASeq := mk 1 2
+
+-- generate n terms of a sequence
+def terms (s : ASeq) (n : Nat) : List Nat := List.range n |>.map λ i =>ap s i
+
+#eval terms ids 10  -- [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+#eval terms evens 10  -- [0, 2, 4, 6, 8, 10, 12, 14, 16, 18]
+#eval terms odds 10  -- [1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
+
+-- use sequence like a function
+instance : CoeFun ASeq fun _ => Nat → Nat := ⟨ap⟩
+
+#eval evens 10  -- 20
 
 def partition (s : ASeq) (n : Nat) : List ASeq :=
   List.range n |>.map fun i => compose s $ mk i n
@@ -35,8 +50,6 @@ instance : ToString ASeq where
     else if s.d == 0 then s!"{s.k}"
     else if s.d == 1 then s!"{s.k} + n"
     else s!"{s.k} + {s.d}n"
-
-def terms (s : ASeq) (n : Nat) : List Nat := List.range n |>.map λ i =>ap s i
 
 -- now for primes ------------------------------------------------
 
@@ -54,7 +67,7 @@ deriving Repr
 instance : ToString PrimeSieve where
   toString s := s!"ps: {s.ps}, pr: {s.pr}, np: {s.np}, ss: {s.ss}"
 
-def init : PrimeSieve := { ps := [], pr := 1, np := ⟨2,Nat.prime_two⟩, ss := [ids] }
+def init : PrimeSieve := { ps := [], pr := 1, np := ⟨2,Nat.prime_two⟩, ss := [mk 2 1] }
 
 def step (s0 : PrimeSieve) : PrimeSieve :=
   let ps := s0.ps ++ [s0.np]
