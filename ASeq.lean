f41206570ba469b13ff6fba1000fecde72c9c19a
tangentstorm <michal.wallace@gmail.com>
2024-07-03 15:41:39 -0400

    ASeq: use auto-generated .mk

--- ASeq.lean
+++ ASeq.lean
@@ -11,23 +11,19 @@ instance : Ord ASeq where  -- !! TODO: use with List.mergeSort
     | .eq => compare s1.d s2.d
     | ord => ord
 
--- mk k d = k + d*n
-def mk (k d : Nat) : ASeq := { k := k, d := d }
-
 -- apply formula to n
 def ap (s : ASeq) (n : Nat) : Nat := s.k + s.d * n
 
 -- apply one formula to another: r(n) := s(t(n))
-def compose (s : ASeq) (t : ASeq) : ASeq := mk (s.k + s.d * t.k) (s.d * t.d)
-
--- identity sequence
-def ids : ASeq := mk 0 1
-
-def evens : ASeq := mk 0 2
-def odds : ASeq := mk 1 2
+def compose (s : ASeq) (t : ASeq) : ASeq := .mk (s.k + s.d * t.k) (s.d * t.d)
 
 -- generate n terms of a sequence
 def terms (s : ASeq) (n : Nat) : List Nat := List.range n |>.map λ i =>ap s i
+
+-- identity sequence
+def ids : ASeq := .mk 0 1
+def evens : ASeq := .mk 0 2
+def odds : ASeq := .mk 1 2
 
 #eval terms ids 10  -- [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 #eval terms evens 10  -- [0, 2, 4, 6, 8, 10, 12, 14, 16, 18]
@@ -39,7 +35,7 @@ instance : CoeFun ASeq fun _ => Nat → Nat := ⟨ap⟩
 #eval evens 10  -- 20
 
 def partition (s : ASeq) (n : Nat) : List ASeq :=
-  List.range n |>.map fun i => compose s $ mk i n
+  List.range n |>.map fun i => compose s $ .mk i n
 
 instance : ToString ASeq where
   toString s :=
