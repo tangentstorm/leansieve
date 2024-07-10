4a3b25b572e3c696f3a9e2c1e2745f19ee1c6ed7
tangentstorm <michal.wallace@gmail.com>
2024-07-10 16:12:24 -0400

    clean up lakefile

--- lakefile.lean
+++ lakefile.lean
@@ -1,24 +1,18 @@
 import Lake
 open Lake DSL
 
-package treesiv {
-  -- add package configuration options here
-}
+package treesiv {}
 
-lean_lib Treesiv {
-  -- add library configuration options here
-}
-
+lean_lib Treesiv {}
 lean_lib PrimeGen { }
 lean_lib PrimeSieve { }
 lean_lib ASeq { }
 lean_lib Rake { }
 lean_lib RakeSieve { }
 
---@[defaultTarget]
---lean_exe treesiv {
---  root := `Main
---}
+@[default_target]
+lean_exe treesiv {
+  root := `Main}
 
 require mathlib from git
   "https://github.com/leanprover-community/mathlib4"
