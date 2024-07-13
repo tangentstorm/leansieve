d6f7f9f4a8df23ae2d475a09c89ab082e6d6addf
tangentstorm <michal.wallace@gmail.com>
2024-07-13 17:58:04 -0400

    RakeSieve: move assertions into the structure itself.

--- RakeSieve.lean
+++ RakeSieve.lean
@@ -7,34 +7,34 @@ structure RakeSieve where
   rm: RakeMap prop
   p : NPrime               -- the current prime
   c : Nat                  -- the canditate for next prime
+  hCinR : c ∈ R p
+  hRmin : ∀ r ∈ R p, c ≤ r
   -- Q : Array RakeMap     -- queue of found primes
 
 def RakeSieve.init : RakeSieve :=
   { prop := λ n => True ∧ n ≥ 2,
     rm := idrm.gte 2,
     p := ⟨2, Nat.prime_two⟩,
-    c := 3}
+    c := 3,
+    hCinR := sorry
+    hRmin := sorry }
 
 def RakeSieve.next (x : RakeSieve) (hC: Nat.Prime x.c) : RakeSieve :=
   let p := x.c
   let c := p + 1 -- TODO: actual next prime
   let prop := λn => x.prop n ∧ ¬ x.c ∣ n
   let rm := x.rm.rem x.c
-  { prop := prop, rm := rm, c := c, p := ⟨p, hC⟩ }
+  { prop := prop, rm := rm, c := c, p := ⟨p, hC⟩,
+    hCinR := sorry,
+    hRmin := sorry }
 
 instance : SieveState RakeSieve where
   P x := x.p
   C x := x.c
   next x hC := RakeSieve.next x hC
   hNext := by aesop
-
 open SieveState
 
-theorem RakeSieve.hCinR  (g:RakeSieve) : C g ∈ R (P g)            -- C is an element of R
-  := sorry
-theorem RakeSieve.hRmin  (g:RakeSieve) : ∀ n ∈ R (P g), C g ≤ n   -- C is min of R
-  := sorry
-
 instance : PrimeSieve RakeSieve where
-  hCinR := RakeSieve.hCinR
-  hRmin := RakeSieve.hRmin
+  hCinR x := by dsimp[C]; dsimp[P]; exact x.hCinR
+  hRmin x := by dsimp[C]; dsimp[P]; exact x.hRmin
