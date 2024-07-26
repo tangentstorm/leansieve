d39c68e8875c57425c6c27d0ce5921fe7ef0e50c
tangentstorm <michal.wallace@gmail.com>
2024-07-26 01:32:35 -0400

    Rake: remove unfinished ascending_terms lemma as it's neither true nor necessary

--- Rake.lean
+++ Rake.lean
@@ -120,43 +120,6 @@ theorem div_lt_of_lt_mod_eq {m n d:Nat} {hdpos: 0 < d} {hmn: m<n} : (m%d = n%d) 
   rw[←m.div_add_mod d, ←n.div_add_mod d, hmod, Nat.add_lt_add_iff_right] at hmn
   exact (Nat.mul_lt_mul_left hdpos).mp hmn
 
--- Example of a rake that is non-ascending.
-def r10 : Rake := { d:=10, ks:=[1,21], hsort:=by simp, hsize:=by simp }
-assert (List.range 4 |>.map r10.term) = [1, 21, 11, 31]
-
-theorem Rake.ascending_terms (r:Rake) (hks: ∀k∈ r.ks, k<r.d) (hdpos: 0 < r.d) {m n : Nat} (hmn: m < n)
- : r.term m < r.term n := by
-  unfold term aseq ASeq.term; simp
-  set kl := r.ks.length
-  set mq := m / kl; set mr := m % kl
-  set nq := n / kl; set nr := n % kl
-  have hmr : mr < kl := by dsimp[mr,kl]; exact Nat.mod_lt m r.hsize
-  have hnr : nr < kl := by dsimp[nr,kl]; exact Nat.mod_lt n r.hsize
-  show r.ks[mr]'hmr + r.d * mq < r.ks[nr]'hnr + r.d * nq
-  by_cases hreq : mr = nr
-  case pos =>
-    -- two terms of the same sequence
-    have : r.ks[mr]'hmr = r.ks[nr]'hnr := by simp_all
-    simp[this, Nat.mul_lt_mul_left, hdpos]
-    dsimp[mq, nq]
-    have hklpos: 0 < kl := r.hsize
-    dsimp[mr,nr] at hreq
-    exact @div_lt_of_lt_mod_eq m n kl hklpos hmn hreq
-  case neg =>
-    -- mr≠nr means two different sequences.
-    -- if mq = nq, we're just adding different coefficients
-    -- if mq < nq, the sequences differ by at least a multiple of d
-    -- !! does it matter that ks[i] can be > d ?
-    have : mq ≤ nq := by dsimp[mq,nq];  sorry
-    have : ∃k, mq + k = nq := by use nq-mq; omega
-    obtain ⟨k,ksum⟩ := this
-    rw[←ksum]
-    conv => rhs; rhs; simp[Nat.mul_add, Nat.add_comm]
-    conv => rhs; rw[←Nat.add_assoc]
-    rw[add_lt_add_iff_right]
-    have : r.ks[mr] < r.ks[nr] := by sorry -- consequence of r.hsort
-    exact Nat.lt_add_right (r.d * k) this
-
 lemma Rake.term_simp (r:Rake) (n:Nat)
 : (∃k, (r.term n = k + r.d * (n/r.ks.length)) ∧ ∃i: Fin r.ks.length, i=n%r.ks.length ∧ k=r.ks[i]) := by
   let i: Fin r.ks.length := ⟨n%r.ks.length, Nat.mod_lt n r.hsize⟩
@@ -179,10 +142,10 @@ lemma sorted_get (xs:List Nat) (hxs: List.Sorted (·<·) xs) (i j:Fin xs.length)
   simp_all
 
 /--
-As currently defined, the terms of a rake can be non-ascending in one of two ways:
-- will have an ascending sawtooth pattern if ∃k∈r.ks, k>r.d
-- will be cyclic (and possibly constant) if r.d=0
-But in all cases, term 0 is the minimum. -/
+As currently defined, the terms of a sorted rake can be non-ascending in one of two ways:
+- ascending sawtooth pattern if ∃k∈r.ks, k>r.d
+- cyclic (and possibly constant) if r.d=0
+But in all cases, if the rake is sorted, term 0 is the minimum. -/
 theorem Rake.sorted_min_term_zero (r: Rake) (hr: r.sorted) : ∀ n, (r.term 0 ≤ r.term n) := by
   intro n
   obtain ⟨k₀, hk₀, i₀, hi₀⟩ := r.term_simp 0
